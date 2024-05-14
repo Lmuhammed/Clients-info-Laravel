a147a7d6f30645a3cbc0dbfdd34752daebcd9eaf
LM47 <140099085+LM-47@users.noreply.github.com>
2024-05-14 01:25:07 +0100

    Item,Supplier controller migration & model added ; Supplier CRUD views and logic added

---
+++ app/Models/Item.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'price_per_item',
+        'number',
+        'items_price',
+        'supplier_id',
+    ];
+
+    public function Supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    } 
+
+}
